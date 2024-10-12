651317023d5a221f274b0bcf8e6b2ef08ba37f5b
garogoz <garogoz.dev@gmail.com>
2024-10-11 23:10:32 -0500

    added form to get the season/year anime, removed allseasons.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, jsonify, request
+from flask import Flask, render_template, redirect, jsonify, request, url_for
 from graph import cache
 import graph
 
@@ -18,7 +18,7 @@ def index():
 
 @app.route('/seasons')
 def seasons():
-    return render_template('allseasons.html', title="Seasons")
+    return redirect(f'/winter/2024')
 
 @app.route('/anime', methods=['GET'])
 def anime():
@@ -26,25 +26,29 @@ def anime():
     return render_template('anime.html', title="Anime", data=data)
 
 
-@app.route('/<season>/<int:year>')
-@app.route('/<season>/<int:year>/<int:page>')
+@app.route('/<season>/<int:year>', methods=['GET'])
+@app.route('/<season>/<int:year>/<int:page>', methods=['GET'])
 def anime_season(season, year, page=0):
-    if page is None:
-         return redirect(f'/{season}/{year}/1')
-    cache_key = f"{season.lower()}-{year}-{page}"
-    cached_data = cache.get(cache_key)
-    title = f'{season.capitalize()} {year}'
-    if cached_data is not None:
-        print("CACHED!")
-        return render_template('season.html', title=title, data=cached_data)
+        if page is None:
+            return redirect(f'/{season}/{year}/1')
+        cache_key = f"{season.lower()}-{year}-{page}"
+        cached_data = cache.get(cache_key)
+        title = f'{season.capitalize()} {year}'
+        if cached_data is not None:
+            print("CACHED!")
+            return render_template('season.html', title=title, data=cached_data)
 
-    # If data is not cached, retrieve it and cache it
-    print("NOT CACHED!!!")
-    season_data = graph.get_anime_season(season, year, page)
-    cache.set(cache_key, season_data)
-    return render_template('season.html',title=title, data=season_data)
+        # If data is not cached, retrieve it and cache it
+        print("NOT CACHED!!!")
+        season_data = graph.get_anime_season(season, year, page)
+        cache.set(cache_key, season_data)
+        return render_template('season.html',title=title, data=season_data)
 
-
+@app.route('/gotoseason', methods=['POST'])
+def gotoseason():
+    season = request.form['season']
+    year = request.form['yearint']
+    return redirect(f'{season}/{year}')
 
 
 
